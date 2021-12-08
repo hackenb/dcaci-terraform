11853de0a17318e8c803259eab8609ef689044f5
Alexander Hackenberg <hacki@hacki.at>
2021-12-08 10:41:41 +0000

    added rs-ctx to bd

--- bd.tf
+++ bd.tf
@@ -2,4 +2,6 @@ resource "aci_bridge_domain" "terra-bd" {
     tenant_dn                   = aci_tenant.terra-tn.id
     description                 = "from terraform"
     name                        = "terra-bd"
+    relation_fv_rs_ctx          = aci_vrf.terra-vrf.name
+
 }
